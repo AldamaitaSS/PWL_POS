c49dbded01ec002a3638d8764d7dc5742f54f593
AldamaitaSS <pijarheksa02@gmail.com>
2024-09-18 11:48:34 +0700

    Jobsheet 4_Praktikum 2.5

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,89 +9,50 @@ use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
     public function index(){
-        // $data = [
-        //     'level_id' => 2,
-        //     'username' => 'manager_tiga',
-        //     'nama' => 'Manager 3',
-        //     'password' => Hash::make('12345'),
-        // ];
-        // UserModel::create($data);
-
-        // $user = UserModel::all();
-        // return view('user', ['data' => $user]);
-
-        // PRAK 2.1
-        // $user = UserModel::find(1);
-        // return view('user', ['data' => $user]);
         
-        // $user = UserModel::where('level_id', 1) -> first();
-        // return view('user', ['data' => $user]);
+        // PRAK 2.5
+        $user = UserModel::create([
+            'username' => 'manager11',
+            'nama' => 'Manager11',
+            'password' => Hash::make('12345'),
+            'level_id' => 2,
+        ]);
 
-        // $user = UserModel::firstWhere('level_id', 1);
-        // return view('user', ['data' => $user]);
+        $user->username = 'manager12';
 
-        // $user = UserModel::findOr(1, ['username', 'nama'], function() {
-        //     abort(404);
-        // });
-        // return view('user', ['data' => $user]);
-
-        // $user = UserModel::findOr(20, ['username', 'nama'], function() {
-        //     abort(404);
-        // });
-        // return view('user', ['data' => $user]);
-
-        //PRAK 2.2
-        // $user = UserModel::findOrFail(1);
-        // return view('user', ['data' => $user]);
-
-        // $user = UserModel::where('username', 'manager9') ->firstOrFail();
-        // return view('user', ['data' => $user]);
-
-        // PRAK 2.3
-        // $user = UserModel::where('level_id', 2)->count();
-        // dd($user);
-        // return view('user', ['data' => $user]);
-
-        // $userCount = UserModel::where('level_id', 2)->count();
-        // return view('user', ['userCount' => $userCount]);
-
-        //PRAK 2.4
-        // $user = UserModel::firstOrcreate(
-        //     [
-        //         'username' => 'manager',
-        //         'nama' => 'Manager',
-        //     ],
-        // );
-        // return view('user', ['data' => $user]);
-
-        // $user = UserModel::firstOrCreate(
-        //     [
-        //         'username' => 'manager22',
-        //         'nama' => 'Manager Dua Dua',
-        //         'password' => Hash::make('12345'),
-        //         'level_id' => 2,
-        //     ],
-        // );
-       
-        // return view('user', ['data' => $user]);
-
-        // $user = UserModel::firstOrNew(
-        //     [
-        //         'username' => 'manager',
-        //         'nama' => 'Manager',
-        //     ],
-        // );
-        // return view('user', ['data' => $user]);
-
-        $user = UserModel::firstOrNew(
-            [
-                'username' => 'manager33',
-                'nama' => 'Manager Tiga Tiga',
-                'password' => Hash::make('12345'),
-                'level_id' => 2,
-            ],
-        );
         $user->save();
-        return view('user', ['data' => $user]);
+
+        $user->wasChanged(); //true
+        $user->wasChanged('username'); //true
+        $user->wasChanged(['nama', 'username']); //true
+        $user->wasChanged('nama'); //false
+        dd($user->wasChanged(['nama', 'username'])); //true
+
+
+        // $user = UserModel::create([
+        //     'username' => 'manager55',
+        //     'nama' => 'Manager55',
+        //     'password' => Hash::make('12345'),
+        //     'level_id' => 2,
+        // ]);
+
+        // $user->username = 'manager56';
+
+        // $user->isDirty(); //true
+        // $user->isDirty('username'); //true
+        // $user->isDirty('nama'); //false
+        // $user->isDirty(['nama', 'username']); //true
+
+        // $user->isClean(); //false
+        // $user->isClean('username'); //false
+        // $user->isClean('nama'); //true
+        // $user->isClean(['nama', 'username']); //false
+
+        // $user->save(); //simpan data ke database
+
+        // $user->isDirty();//false
+        // $user->isClean(); //true
+        // dd($user->isDirty());
+
     }
 }
