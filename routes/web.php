fb1b6a0f1a49b5f2bccab1cbfa5c22740127c167
AldamaitaSS <pijarheksa02@gmail.com>
2024-09-25 11:49:58 +0700

    Jobsheet 5_Praktikum 2

--- routes/web.php
+++ routes/web.php
@@ -1,38 +1,21 @@
 <?php
 
-use App\Http\Controllers\KategoriController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LevelController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\WelcomeController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+// index
+Route::get('/', [WelcomeController::class, 'index']);
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['prefix' => 'user'], function(){
+    Route::get('/', [UserController::class, 'index']);
+    Route::post('/list', [UserController::class, 'list']);
+    Route::get('/create', [UserController::class,'create']);
+    Route::post('/', [UserController::class, 'store']);
+    Route::get('/{id}', [UserController::class,'show']);
+    Route::get('/{id}/edit', [UserController::class,'edit']);
+    Route::put('/{id}', [UserController::class, 'update']);
+    Route::delete('/{id}', [UserController::class, 'destroy']);
 });
 
-// level
-Route::get('/level', [LevelController::class, 'index']);
-// kategori
-Route::get('/kategori', [KategoriController::class, 'index']);
-//user
-Route::get('/user', [UserController::class, 'index']);
-//user tambah
-Route::get('/user/tambah', [UserController::class, 'tambah']);
-//tambah simpan
-Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
-//ubah
-Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
-//ubah simpan
-Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
-//delete
-Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
