881a0e0c0c226090346a8bc67ee6727ebdc92691
AldamaitaSS <pijarheksa02@gmail.com>
2024-09-27 08:19:35 +0700

    Jobsheet 5_Praktikum 3

--- resources/views/layouts/breadcrumb.blade.php
+++ resources/views/layouts/breadcrumb.blade.php
@@ -1,11 +1,21 @@
+<?php
+// Inisialisasi $breadcrumb dengan properti title dan list
+$breadcrumb = (object) [
+    'title' => 'Judul Halaman', // Berikan judul halaman sesuai keperluan
+    'list' => ['Beranda', 'Bagian', 'Halaman'] // Daftar breadcrumb
+];
+?>
+
 <section class="content-header">
     <div class="container-fluid">
         <div class="row mb-2">
-            <div class="col-sm-6"><h1>{{ $breadcrumb->title }}</h1></div>
+            <div class="col-sm-6">
+                <h1>{{ $breadcrumb->title }}</h1>
+            </div>
             <div class="col-sm-6">
                 <ol class="breadcrumb float-sm-right">
-                    @foreach($breadcrumb->list as $key => $value)
-                        @if($key == count($breadcrumb->list) - 1)
+                    @foreach ($breadcrumb->list as $key => $value)
+                        @if ($key == count($breadcrumb->list) - 1)
                             <li class="breadcrumb-item active">{{ $value }}</li>
                         @else
                             <li class="breadcrumb-item">{{ $value }}</li>
@@ -16,3 +26,4 @@
         </div>
     </div>
 </section>
+
--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -5,12 +5,20 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>{{ config('app.name', 'PWL Laravel Starter COde') }}</title>
 
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+  
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome -->
   <link rel="stylesheet" href="{{ asset('AdminLTE/plugins/fontawesome-free/css/all.min.css') }}">
+  <!-- DataTables -->
+  <link rel="stylesheet" href="{{ asset('AdminLTE/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('AdminLTE/plugins/datatables-responsive/css/responsive.bootstrap4.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('AdminLTE/plugins/datatables-buttons/css/buttons.bootstrap4.min.css') }}">"
   <!-- Theme style -->
   <link rel="stylesheet" href="{{ asset('AdminLTE/dist/css/adminlte.min.css') }}">
+
+  @stack('css')
 </head>
 <body class="hold-transition sidebar-mini">
 <!-- Site wrapper -->
@@ -23,7 +31,7 @@
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="{{ url('/') }}" class="brand-link">
-      <img src="{{ assets('AdminLTE/dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <img src="{{ asset('AdminLTE/dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">PWL - Starter Code</span>
     </a>
 
@@ -35,7 +43,7 @@
   <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
     <!-- Content Header (Page header) -->
-    @include('layout.breadcrumb')
+    @include('layouts.breadcrumb')
 
     <!-- Main content -->
     <section class="content">
@@ -53,7 +61,25 @@
 <script src="{{ asset('AdminLTE/plugins/jquery/jquery.min.js') }}"></script>
 <!-- Bootstrap 4 -->
 <script src="{{ asset('AdminLTE/plugins/bootstrap/js/bootstrap.blundle.min.js') }}"></script>
+<!-- DataTables & Plugins -->
+<script src="{{ asset('AdminLTE/plugins/dataTables/jquery.dataTables.min.js') }}"></script>
+<script src="{{ asset('AdminLTE/plugins/dataTables-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
+<script src="{{ asset('AdminLTE/plugins/datatables-responsive/js/dataTables.responsive.min.js') }}"></script>
+<script src="{{ asset('AdminLTE/plugins/datatables-responsive/js/responsive.bootstrap4.min.js') }}"></script>
+<script src="{{ asset('AdminLTE/plugins/datatables-buttons/js/dataTables.buttons.min.js') }}"></script>
+<script src="{{ asset('AdminLTE/plugins/datatables-buttons/js/buttons.bootstrap4.min.js') }}"></script>
+<script src="{{ asset('AdminLTE/plugins/jszip/jszip.min.js') }}"></script>
+<script src="{{ asset('AdminLTE/plugins/pdfmake/pdfmake.min.js') }}"></script>
+<script src="{{ asset('AdminLTE/plugins/pdfmake/vfs_font.js') }}"></script>
+<script src="{{ asset('AdminLTE/plugins/datatables-buttons/js/buttons.html5.min.js') }}"></script>
+<script src="{{ asset('AdminLTE/plugins/datatables-buttons/js/buttons.print.min.js') }}"></script>
+<script src="{{ asset('AdminLTE/plugins/datatables-buttons/js/buttons.colVis.min.js') }}"></script>
 <!-- AdminLTE App -->
 <script src="{{ asset('AdminLTE/dist/js/adminlte.min.js') }}"></script>
+<script>
+    $.ajaxSetup({headers: {'X-CSRF-TOKEN': $('meta[name="csrf_token"]').attr('content')}});
+</script>
+@stack('js')
 </body>
 </html>
+
